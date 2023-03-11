4e7d6f85de00677912ed6ad82646fa7024482597
RZhang <richardzhiyuanzhang@gmail.com>
2023-03-11 18:37:19 -0500

    add all_receipts_by_user and filtered_receipts receipt queries to receipts schema. update names and logic of existing receipt queries

--- server/receipts/graphql/receipts/schema.py
+++ server/receipts/graphql/receipts/schema.py
@@ -1,10 +1,20 @@
 import graphene
 import cloudinary.uploader
-from graphene_django.types import DjangoObjectType, ObjectType
+import operator
+
 from ...models import Receipt
+from django.contrib.auth import get_user_model
+from django.db.models import QuerySet
+from django.db.models import Q
+from functools import reduce
+
+from cloudinary.models import CloudinaryField
+
+from graphql import GraphQLError
+from graphene_django.types import DjangoObjectType, ObjectType
 from graphene_file_upload.scalars import Upload
 from graphene_django.converter import convert_django_field
-from cloudinary.models import CloudinaryField
+
 
 # Register a custom converter for CloudinaryField
 
@@ -15,6 +25,13 @@ def convert_field_to_string(field, registry=None):
     Converter for CloudinaryField to be used by Graphene.
     """
     return graphene.String()
+
+
+def filter_queryset(qs: QuerySet, condition, error_msg: str) -> QuerySet:
+    filtered_qs = qs.filter(**condition)
+    if not filtered_qs.exists():
+        raise GraphQLError(error_msg)
+    return filtered_qs
 
 
 class ReceiptType(DjangoObjectType):
@@ -29,23 +46,173 @@ class ReceiptType(DjangoObjectType):
 
 
 class Query(ObjectType):
-    receipt = graphene.Field(ReceiptType, id=graphene.Int())
-    receipts = graphene.List(ReceiptType)
+    receipt = graphene.Field(ReceiptType, id=graphene.ID(required=True))
+    all_receipts = graphene.List(ReceiptType)
+    all_receipts_by_user = graphene.List(
+        ReceiptType,
+        user_id=graphene.ID(required=True)
+    )
+    filtered_receipts = graphene.List(
+        ReceiptType,
+        user_id=graphene.ID(),
+        store_name=graphene.String(),
+        expense=graphene.String(),
+        date_gte=graphene.Date(),
+        date_lte=graphene.Date(),
+        cost_gte=graphene.Decimal(),
+        cost_lte=graphene.Decimal(),
+        tax_gte=graphene.Decimal(),
+        tax_lte=graphene.Decimal(),
+        notes=graphene.String(),
+        tags_contains_any=graphene.String(),
+        tags_contains_all=graphene.String(),
+    )
 
-    def resolve_receipt(self, info, **kwargs):
-        id = kwargs.get('id')
-
-        if id is not None:
+    def resolve_receipt(self, info, id):
+        try:
             return Receipt.objects.get(pk=id)
+        except:
+            return GraphQLError(f'No receipt with id: {id} found.')
 
-        return None
+    def resolve_all_receipts(self, info, **kwargs):
+        try:
+            return Receipt.objects.all()
+        except:
+            return GraphQLError('No receipts found.')
 
-    def resolve_receipts(self, info, **kwargs):
-        return Receipt.objects.all()
+    def resolve_all_receipts_by_user(self, info, user_id):
+        try:
+            user = get_user_model().objects.get(pk=user_id)
+        except:
+            return GraphQLError(f'No user with user id: {user_id} found.')
+
+        receipts = Receipt.objects.filter(user=user)
+
+        if receipts:
+            return receipts
+        else:
+            return GraphQLError(f'No receipts found for user with user id: {user_id}.')
+
+    def resolve_filtered_receipts(self, info, **kwargs):
+        if not kwargs:
+            return GraphQLError('Please provide at least one field to filter by. Fields include: userId, storeName, expense, dateGte, dateLte, costGte, costLte, notes, and tags')
+
+        queryset = Receipt.objects.all()
+        if not queryset:
+            return GraphQLError('No receipts found in the database.')
+
+        user_id = kwargs.get('user_id')
+        store_name = kwargs.get('store_name')
+        expense = kwargs.get('expense')
+        date_gte = kwargs.get('date_gte')
+        date_lte = kwargs.get('date_lte')
+        cost_gte = kwargs.get('cost_gte')
+        cost_lte = kwargs.get('cost_lte')
+        tax_gte = kwargs.get('tax_gte')
+        tax_lte = kwargs.get('tax_lte')
+        notes = kwargs.get('notes')
+        tags_contains_any = kwargs.get('tags_contains_any')
+        tags_contains_all = kwargs.get('tags_contains_all')
+
+        if user_id:
+            try:
+                user = get_user_model().objects.get(pk=user_id)
+                queryset = queryset.filter(user=user)
+            except:
+                raise GraphQLError(f"No user found with id {user_id}")
+
+        if store_name:
+            queryset = filter_queryset(
+                queryset, 
+                {'store_name__icontains': store_name},
+                'No receipts found with the specified store name.'
+            )
+
+        if expense:
+            queryset = filter_queryset(
+                queryset, 
+                {'expense': expense}, 
+                'No receipts found with the specified expense.'
+            )
+
+        if date_gte:
+            queryset = filter_queryset(
+                queryset, 
+                {'date__gte': date_gte}, 
+                'No receipts found with a date greater than or equal to the specified date.'
+            )
+
+        if date_lte:
+            queryset = filter_queryset(
+                queryset, 
+                {'date__lte': date_lte}, 
+                'No receipts found with a date less than or equal to the specified date.'
+            )
+
+        if cost_gte:
+            queryset = filter_queryset(
+                queryset, 
+                {'cost__gte': cost_gte}, 
+                'No receipts found with a cost greater than or equal to the specified cost.'
+            )
+
+        if cost_lte:
+            queryset = filter_queryset(
+                queryset, 
+                {'cost__lte': cost_lte}, 
+                'No receipts found with a cost less than or equal to the specified cost.'
+            )
+
+        if tax_gte:
+            queryset = filter_queryset(
+                queryset, 
+                {'tax__gte': tax_gte}, 
+                'No receipts found with a tax greater than or equal to the specified tax.'
+            )
+
+        if tax_lte:
+            queryset = filter_queryset(
+                queryset, 
+                {'tax__lte': tax_lte}, 
+                'No receipts found with a tax less than or equal to the specified tax.'
+            )
+
+        if notes:
+            notes = notes.split()
+            query = reduce(
+                operator.or_, 
+                (Q(notes__icontains=note) for note in notes)
+            )
+            queryset = filter_queryset(
+                queryset, 
+                {'query': query}, 
+                'No receipts found with the specified notes.'
+            )
+
+        if tags_contains_any:
+            tags = tags_contains_any.split()
+            queryset = filter_queryset(
+                queryset, 
+                {'tags__tag_name__in': tags}, 
+                'No receipts found with the specified tags.'
+            )
+
+        if tags_contains_all:
+            tags = tags_contains_all.split()
+            query = reduce(
+                operator.and_, 
+                (Q(tags__tag_name__icontains=tag) for tag in tags)
+            )
+            queryset = filter_queryset(
+                queryset, 
+                {'query': query}, 
+                'No receipts found with all of the specified tags.'
+            )
+
+        return queryset
 
 
 class ReceiptInput(graphene.InputObjectType):
-    id = graphene.ID()
     user_id = graphene.ID()
     store_name = graphene.String()
     date = graphene.Date()
@@ -102,7 +269,7 @@ class UpdateReceipt(graphene.Mutation):
             if receipt_instance.receipt_image:
                 if receipt_data.receipt_image:
                     public_id = receipt_instance.image_public_id()
-                    cloudinary.uploader.destroy(public_id)                    
+                    cloudinary.uploader.destroy(public_id)
                 receipt_instance.receipt_image = receipt_data.receipt_image
             try:
                 receipt_instance.save()
