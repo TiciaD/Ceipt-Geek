4adddcb4c6692f3a1405480c910e7b5a90a51bfe
RZhang <richardzhiyuanzhang@gmail.com>
2023-03-29 00:41:07 -0400

    install and integrate cockroachdb with backend

--- server/core/settings.py
+++ server/core/settings.py
@@ -93,11 +93,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django_cockroachdb',
+        'NAME': os.environ.get('COCKROACH_DB_DATABASE'),
+        'USER': os.environ.get('COCKROACH_DB_USERNAME'),
+        'PASSWORD': os.environ.get('COCKROACH_DB_USER_PASSWORD'),
+        'HOST': os.environ.get('COCKROACH_DB_HOST'),
+        'PORT': os.environ.get('COCKROACH_DB_PORT'),
+        'OPTIONS': {
+            'sslmode': 'verify-full'
+        },
+    },
 }
 
 # Cloudinary Config
