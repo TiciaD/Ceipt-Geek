43b9fb7875a2e3b2068e13949597febf5f665a74
RZhang <richardzhiyuanzhang@gmail.com>
2023-03-28 21:06:29 -0400

    redirect root url to /admin

--- server/core/urls.py
+++ server/core/urls.py
@@ -24,7 +24,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', RedirectView.as_view(url='/api')),
+    path('', RedirectView.as_view(url='/admin')),
     path('api/', include('receipts.urls')),
     path('graphql/', csrf_exempt(FileUploadGraphQLView.as_view(graphiql=True))),
 ]
