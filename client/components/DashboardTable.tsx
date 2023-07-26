05c6c80bafdd11afde7be5e35481b856c4b5fd23
RZhang <richardzhiyuanzhang@gmail.com>
2023-07-25 20:12:27 -0400

    change receipt placeholder image. change data table chips.

--- client/components/DashboardTable.tsx
+++ client/components/DashboardTable.tsx
@@ -173,7 +173,6 @@ export default function ReceiptsTable() {
               return (
                 <Grid item>
                   <Chip
-                    variant="outlined"
                     label={tag}
                     sx={{
                       borderColor:
